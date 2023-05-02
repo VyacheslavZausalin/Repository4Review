a02167f87a8a327519478f18e601f7c87b1c1c60
vaceslavzausalin <zausalin90@mail.ru>
2023-05-02 22:20:13 +0300

    new_examples

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def test_is_button_there_multy_language(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(10)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button, "No button!!!!"
